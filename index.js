6484cdb2f467278533f996ab8f4e0cfda18e4eed
Greyarch BV <gantcho@greyarch.net>
2018-03-06 10:37:40 +0100

    Pulled out the labeled locator to its own package

--- index.js
+++ index.js
@@ -17,31 +17,9 @@ const locator = type => (...values) => ({
   value: values
 });
 
-const labeled = function() {
-  protractor.By.addLocator("labeled", function(labelText, parent, root) {
-    const using = parent || document;
-    const labels = using.querySelectorAll("label");
-    const labelsWithText = Array.prototype.filter.call(
-      labels,
-      label => label.textContent === labelText
-    );
-    return Array.prototype.map.call(labelsWithText, label =>
-      using.getElementById(
-        label != null ? label.getAttribute("for") : undefined
-      )
-    );
-  });
-
-  return label => ({
-    locator: "labeled",
-    value: label
-  });
-};
-
 const objects = locators.reduce((acc, loc) => {
   acc[loc] = locator(loc);
   return acc;
 }, {});
-objects["labeled"] = labeled();
 
 module.exports = objects;
